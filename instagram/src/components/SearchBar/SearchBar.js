95afb791aa9c2c99b63fd71986f5425f2fd38774
Mychal Hall <hallmychal@gmail.com>
2019-03-13 21:14:06 -0500

    app structure mostly built

---
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SearchBar = props => {
+    return(
+        <div>
+            <h1>Hello from SearchBar?</h1>
+        </div>
+    )
+}
+
+export default SearchBar;
