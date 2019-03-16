bcc71f04ce59ceb9fe4125369abb5fdea84cbc74
Mychal Hall <hallmychal@gmail.com>
2019-03-16 12:00:45 -0500

    CDM function added to setState

--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -1,11 +1,17 @@
 import React from "react";
 
+import moment from 'moment';
+import './CommentSection.css'
 import Comment from "./Comment";
 
 const CommentSection = props => {
   return (
     <div className="commentSection">
       <Comment comment={props.comments} />
+      <p className='momentTrack'>{moment().fromNow('hh')} ago . . . </p>
+      <form className='newComment'>
+        <input className='addComment' placeholder='Add a comment...'/>
+      </form>
     </div>
   );
 };
