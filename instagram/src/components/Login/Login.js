2ce2beda28bef4de8f1eb9d2f6368d16f0d7274d
Mychal Hall <hallmychal@gmail.com>
2019-03-20 00:18:21 -0500

    My styles are still trash static placeholders, sorry

--- instagram/src/components/Login/Login.js
+++ instagram/src/components/Login/Login.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 
+import "./Login.css";
+import logo from "../img/logo.png";
+
 class Login extends Component {
   constructor(props) {
     super(props);
@@ -17,29 +20,34 @@ class Login extends Component {
     const user = this.state.username;
     localStorage.setItem("user", user);
     const password = this.state.password;
-    localStorage.setItem("password", password)
+    localStorage.setItem("password", password);
     window.location.reload();
   };
 
   render() {
     return (
-      <form>
-        <input
-          type="text"
-          name="username"
-          placeholder="Username"
-          value={this.state.username}
-          onChange={this.changeHandler}
-        />
-        <input
-          type="text"
-          name="password"
-          placeholder="Password"
-          value={this.state.password}
-          onChange={this.changeHandler}
-        />
-        <button onClick={this.loginSubmit}>Login</button>{" "}
-      </form>
+      <div className="loginContainer">
+        <img className="loginLogo" src={logo} alt="Instagram logo" />
+        <form className="loginForm">
+          <input
+            className="loginInput"
+            type="text"
+            name="username"
+            placeholder="Username"
+            value={this.state.username}
+            onChange={this.changeHandler}
+          />
+          <input
+            className="loginInput"
+            type="text"
+            name="password"
+            placeholder="Password"
+            value={this.state.password}
+            onChange={this.changeHandler}
+          />
+          <button className="loginButton" onClick={this.loginSubmit}>Login</button>{" "}
+        </form>
+      </div>
     );
   }
 }
