33bf3b071164e0e026915d107739e7e930d12fef
Mychal Hall <hallmychal@gmail.com>
2019-03-21 22:10:30 -0500

    refactor complete

--- instagram/src/components/CommentSection/Comment.js
+++ instagram/src/components/CommentSection/Comment.js
@@ -2,16 +2,27 @@ import React from "react";
 import PropTypes from "prop-types";
 
 import "./CommentSection.css";
+import styled from 'styled-components';
+
+const CommentHolder = styled.div `
+display: flex;
+margin-left: 10px;`
+
+const NewComment = styled.p `
+margin-top: 0;
+margin-bottom: 8px;
+font-size: 16px;
+`
 
 function Comment(props) {
   return (
     <>
       {props.data.map(comment => (
-        <div className="comment">
-          <p className="commentContent">
+        <CommentHolder>
+          <NewComment className="commentContent">
             <b>{comment.username}</b> {comment.text}
-          </p>
-        </div>
+          </NewComment>
+        </CommentHolder>
       ))}
     </>
   );
--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -3,6 +3,30 @@ import React, { Component } from "react";
 import moment from "moment";
 import "./CommentSection.css";
 import Comment from "./Comment";
+import styled from 'styled-components';
+
+const CommentContainer = styled.div`
+width: 100%;
+margin-top: 0px;
+`
+
+const CommentAdd = styled.input`
+margin-top: 20px;
+width: 90%;
+height: 20px;
+font-size: 16px;
+border: none;
+::placeholder {
+  font-size: 16px;
+  text-align: left;
+}
+`
+const NewComment = styled.form`
+margin-left: 12px;
+width: 95%;
+height: 60px;
+border-top: 1px solid rgb(240, 240, 240);
+`
 
 class CommentSection extends Component {
   constructor(props) {
@@ -35,18 +59,17 @@ class CommentSection extends Component {
 
   render() {
     return (
-      <div className="commentSection">
+      <CommentContainer>
         <Comment data={this.state.comments} />
         <p className="momentTrack">{moment().fromNow("hh")} ago . . . </p>
-        <form onSubmit={this.submitHandler} className="newComment">
-          <input
+        <NewComment onSubmit={this.submitHandler}>
+          <CommentAdd
             value={this.props.text}
             onChange={this.changeHandler}
-            className="addComment"
             placeholder="Add a comment..."
           />
-        </form>
-      </div>
+        </NewComment>
+      </CommentContainer>
     );
   }
 }
